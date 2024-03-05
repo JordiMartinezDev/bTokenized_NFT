fc0ae6a21da2b8a4a44ed8f2a6f667b71beaa38d
JordiMartinezDev <jordiupc87@gmail.com>
2024-03-05 10:39:31 +0100

    Test token minting

--- test/BasicNFTTests.t.sol
+++ test/BasicNFTTests.t.sol
@@ -10,6 +10,9 @@ contract BasicNFTTests is Test{
 
     DeployBasicNFT public deployer;
     BasicNFT public basicNft;
+    address public USER = makeAddr("user");
+
+    string public constant testURI = "https://bafybeiar6p52sbxvkcv5zne4pqmepjuv5xuwmdwu5o42pezb2jv5pxhlmu.ipfs.nftstorage.link/";
 
     function setUp() public {
 
@@ -24,5 +27,13 @@ contract BasicNFTTests is Test{
        assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
     }
     
+    function testTokenMinting() public {
+        vm.prank(USER);
 
+
+        basicNft.mintNft(testURI);
+        assert(basicNft.balanceOf(USER) >= 1);
+
+        assert(keccak256(abi.encodePacked(testURI)) == keccak256(abi.encodePacked(basicNft.tokenURI(0))));
+    }
 }
